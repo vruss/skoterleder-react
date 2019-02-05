5d472510c500887a479b9e22c332f2e02ef9409e
Viktor Rosvall <virosen@yahoo.se>
2019-02-05 23:04:57 +0100

    Implemented marker rendering based on zoom level. TODO: Make clusters

--- src/map/map.jsx
+++ src/map/map.jsx
@@ -1,60 +1,71 @@
 import React from "react";
 import { Map, TileLayer, ZoomControl } from "react-leaflet";
-// import { fetch } from "whatwg-fetch";
-import JsonMarkers from "../data/markers.json";
+import { fetch } from "whatwg-fetch";
+// import JsonMarkers from "../data/markers.json";
 import Markers from "./marker";
 import MarkerClusterGroup from "react-leaflet-markercluster/dist/react-leaflet-markercluster";
 
 import "./map.css";
 
 class MyMap extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			lat: 62.31,
-			lng: 15.39,
-			zoom: 7,
-			markers: []
-		};
-	}
+   constructor(props) {
+      super(props);
+      this.state = {
+         lat: 62.31,
+         lng: 15.39,
+         zoom: 7,
+         markers: [],
+         onlyUserMarkers: true,
+      };
+   }
 
-	// Load markers into state
-	componentDidMount() {
-		this.setState({ markers: JsonMarkers.marker });
+   // Load markers into state
+   componentDidMount() {
+      // this.setState({ markers: JsonMarkers.marker });
+      // "https://skoterleder.org/inc/getmarker.php?id=281"
 
-		// fetch(URL)
-		// 	.then(response => response.json())
-		// 	.then(data => this.setState({ markers: data.marker }));
-	}
+      fetch("https://test.skoterleder.org/inc/data-icon-string.php")
+         .then(response => response.json())
+         .then(data => this.setState({ markers: data.marker }));
+   }
 
-	render() {
-		const maxBoundsLatLng = [[40.712, -74.227], [40.774, -74.125]];
-		const center = [this.state.lat, this.state.lng];
+   // Called when user zooms
+   handleZoomed = e => {
+      this.setState({ onlyUserMarkers: e.target._zoom < 9 ? true : false });
+   };
 
-		return (
-			// Create the map object
-			<Map
-				center={center}
-				zoom={this.state.zoom}
-				animate={true}
-				minZoom={5}
-				maxZoom={14}
-				zoomControl={false}
-			>
-				{/* Render the map tiles */}
-				<TileLayer
-					MaxBounds={maxBoundsLatLng}
-					attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-					url="https://tiles.skoterleder.org/tiles/{z}/{x}/{y}.png"
-				/>
-				{/* Render the markers */}
-				<MarkerClusterGroup>
-					<Markers markers={this.state.markers} />
-				</MarkerClusterGroup>
-				<ZoomControl position="topright" />
-			</Map>
-		);
-	}
+   render() {
+      const maxBoundsLatLng = [[40.712, -74.227], [40.774, -74.125]];
+      const center = [this.state.lat, this.state.lng];
+
+      return (
+         // Create the map object
+         <Map
+            center={center}
+            zoom={this.state.zoom}
+            animate={true}
+            minZoom={5}
+            maxZoom={14}
+            zoomControl={false}
+            onZoomend={this.handleZoomed}
+         >
+            {/* Render the map tiles */}
+            <TileLayer
+               MaxBounds={maxBoundsLatLng}
+               attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+               url="https://tiles.skoterleder.org/tiles/{z}/{x}/{y}.png"
+            />
+            {/* Render the user markers */}
+            <MarkerClusterGroup maxClusterRadius={70}>
+               <Markers
+                  markers={this.state.markers}
+                  onlyUserMarkers={this.state.onlyUserMarkers}
+               />
+            </MarkerClusterGroup>
+            <ZoomControl position="topright" />
+         </Map>
+      );
+   }
 }
 
 export default MyMap;
--- src/map/marker.jsx
+++ src/map/marker.jsx
@@ -3,39 +3,90 @@ import { Marker } from "react-leaflet";
 import L from "leaflet";
 
 export default function MyMarker(props) {
-	// Returns a marker with different anchor points depending on the marker type
-	function getIcon(marker) {
-		// eslint-disable-next-line
-		let icon;
-		if (
-			marker.icon === "fuel" ||
-			marker.icon === "shelter" ||
-			marker.icon === "wildernesshut"
-		) {
-			return (icon = L.icon({
-				iconUrl: require(`../images/icons/${marker.icon}.png`),
-				iconAnchor: [10, 10],
-				iconSize: [20, 20],
-				popupAnchor: [17, 0]
-			}));
-		} else {
-			return (icon = L.icon({
-				iconUrl: require(`../images/icons/${marker.icon}.png`),
-				iconAnchor: [16, 37],
-				iconSize: [32, 37],
-				popupAnchor: [0, -33]
-			}));
-		}
-	}
+   // Iterate through each marker in the array
+   return props.markers.map(marker => {
+      return getMarker(marker, props);
 
-	// Iterate through each marker in the array
-	return props.markers.map(marker => {
-		return (
-			<Marker
-				key={marker.properties.id}
-				position={marker.coordinates}
-				icon={getIcon(marker)}
-			/>
-		);
-	});
+      // Returns markers if they are user made
+      // return (props.onlyUserMarkers && isUserIcon(marker)) ? (
+      //    <Marker
+      //       key={marker.properties.id}
+      //       position={marker.coordinates}
+      //       icon={getIcon(marker)}
+      //    />
+      // ) : // Else return null
+      // <Marker
+      //       key={marker.properties.id}
+      //       position={marker.coordinates}
+      //       icon={getIcon(marker)}
+      //    />;
+   });
+}
+
+// Returns either all the markers or just user made ones
+function getMarker(marker, props) {
+   // Return user made ones
+   if (props.onlyUserMarkers) {
+      if (isUserIcon(marker)) {
+         return (
+            <Marker
+               key={marker.properties.id}
+               position={marker.coordinates}
+               icon={getIcon(marker)}
+            />
+         );
+      }
+   }
+   // Return all markers
+   else {
+      return (
+         <Marker
+            key={marker.properties.id}
+            position={marker.coordinates}
+            icon={getIcon(marker)}
+         />
+      );
+   }
+
+   // return (
+   //    <Marker
+   //       key={marker.properties.id}
+   //       position={marker.coordinates}
+   //       icon={getIcon(marker)}
+   //    />
+   // );
+}
+
+// Returns true if the icon is user made
+function isUserIcon(marker) {
+   return !(
+      marker.icon === "fuel" ||
+      marker.icon === "shelter" ||
+      marker.icon === "wildernesshut"
+   );
+}
+
+// Returns a marker with different anchor points depending on the marker type
+function getIcon(marker) {
+   // eslint-disable-next-line
+   let icon;
+   if (
+      marker.icon === "fuel" ||
+      marker.icon === "shelter" ||
+      marker.icon === "wildernesshut"
+   ) {
+      return (icon = L.icon({
+         iconUrl: require(`../images/icons/${marker.icon}.png`),
+         iconAnchor: [10, 10],
+         iconSize: [20, 20],
+         popupAnchor: [17, 0],
+      }));
+   } else {
+      return (icon = L.icon({
+         iconUrl: require(`../images/icons/${marker.icon}.png`),
+         iconAnchor: [16, 37],
+         iconSize: [32, 37],
+         popupAnchor: [0, -33],
+      }));
+   }
 }
