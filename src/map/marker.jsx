aa9fcce9046f76abc7033d570d423461ebf43025
Viktor Rosvall <virosen@yahoo.se>
2019-02-05 17:54:16 +0100

    Fixed marker anchor points and scaling depending on marker type

--- src/map/marker.jsx
+++ src/map/marker.jsx
@@ -3,25 +3,39 @@ import { Marker } from "react-leaflet";
 import L from "leaflet";
 
 export default function MyMarker(props) {
-	let icon;
+	// Returns a marker with different anchor points depending on the marker type
+	function getIcon(marker) {
+		// eslint-disable-next-line
+		let icon;
+		if (
+			marker.icon === "fuel" ||
+			marker.icon === "shelter" ||
+			marker.icon === "wildernesshut"
+		) {
+			return (icon = L.icon({
+				iconUrl: require(`../images/icons/${marker.icon}.png`),
+				iconAnchor: [10, 10],
+				iconSize: [20, 20],
+				popupAnchor: [17, 0]
+			}));
+		} else {
+			return (icon = L.icon({
+				iconUrl: require(`../images/icons/${marker.icon}.png`),
+				iconAnchor: [16, 37],
+				iconSize: [32, 37],
+				popupAnchor: [0, -33]
+			}));
+		}
+	}
 
 	// Iterate through each marker in the array
-	return props.markers.map(
-		marker => (
-			// Create the marker icon
-			(icon = L.icon({
-				iconUrl: require(`../images/icons/${marker.icon}.png`),
-				popupAnchor: [0, -33]
-				// eslint-disable-next-line
-			})),
-			(
-				// Render the marker
-				<Marker
-					key={marker.properties.id}
-					position={marker.coordinates}
-					icon={icon}
-				/>
-			)
-		)
-	);
+	return props.markers.map(marker => {
+		return (
+			<Marker
+				key={marker.properties.id}
+				position={marker.coordinates}
+				icon={getIcon(marker)}
+			/>
+		);
+	});
 }
